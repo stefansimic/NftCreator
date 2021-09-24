a8469d1377ee6e54582da678cd236b60430a53cf
Stefan Simic <35811820+stefansimic@users.noreply.github.com>
2021-09-24 12:41:50 +0200

    add todos in model

--- NFTs/src/model/Model.java
+++ NFTs/src/model/Model.java
@@ -11,15 +11,16 @@ public class Model {
 		return assetPath;
 	}
 
+	// TODO rename function. it does not what its name tells
 	public void setAssetPath(String assetPath, NftAssetBlueprint blueprint) {
 		System.out.println(assetPath);
 		this.assetPath = assetPath;
 		this.initImageService();
+		// todo should be cleaner
 		this.imageService.getRandomAsset(NftAssetFactory.createNftAsset(blueprint));
 	}
 	
 	private void initImageService() {
 		this.imageService = new ImageService(this);
 	}
-
 }

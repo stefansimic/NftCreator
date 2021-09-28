3aec2410b03daafb9d9adbcd9309fc0da667be2a
Stefan Simic <35811820+stefansimic@users.noreply.github.com>
2021-09-28 12:47:29 +0200

    add todo for new method

--- NFTs/src/file/RandomService.java
+++ NFTs/src/file/RandomService.java
@@ -5,5 +5,8 @@ import java.io.File;
 import entities.NftAsset;
 
 public interface RandomService {
+	// todo rename createRandomAsset
 	public File getRandomAsset(NftAsset asset, String path);
+	// todo add method createRandomAssets
+	//public void getRandomAsset(NftAsset asset, String path, int howManyTimes);
 }
